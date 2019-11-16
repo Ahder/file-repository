2c80f90fb7779c96592c1f7b6b7f437ee692b77e
Ahder <benzinered@yahoo.fr>
2019-11-16 17:35:09 +0100

    Adding Test class for UserController

---
+++ server/src/test/java/com/rizomm/filemanager/controllers/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.rizomm.filemanager.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnResponseOk() throws Exception {
+        mockMvc.perform(
+                post("/users/login")
+                        .param("username", "user")
+                        .param("password", "pass"))
+                .andExpect(status().isOk());
+    }
+}
