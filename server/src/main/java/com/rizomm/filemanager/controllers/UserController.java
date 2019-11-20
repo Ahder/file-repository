47c5a0d1efca1c27541dcbf01fde3eccba1ad4f8
Miguel Yann <tsambamiguelito@gmail.com>
2019-11-20 08:03:10 +0100

    Update authentification WIP
    Took 53 minutes

--- server/src/main/java/com/rizomm/filemanager/controllers/UserController.java
+++ server/src/main/java/com/rizomm/filemanager/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.rizomm.filemanager.controllers;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.security.Principal;
 
 @Controller
-@RequestMapping("/user")
-
+@EnableOAuth2Client
 public class UserController {
 
-    @GetMapping()
+    @RequestMapping("/user")
     public Principal user(Principal principal){
         return principal;
     }
