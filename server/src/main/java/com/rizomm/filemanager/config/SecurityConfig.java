2cd10ae91c46cda6c83678bd34ad2409660e3439
Ahder <benzinered@yahoo.fr>
2019-11-30 20:58:16 +0100

    Get all uploaded files & download a file

--- server/src/main/java/com/rizomm/filemanager/config/SecurityConfig.java
+++ server/src/main/java/com/rizomm/filemanager/config/SecurityConfig.java
@@ -9,13 +9,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+     /*   http
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt();*/
 
     }
 
