e2b1f3dc663828119bb9c7e96d027bee0a1c0c5c
hichem <hichembedjaoui4@gmail.com>
2019-11-30 15:39:28 +0100

    add okta authentification

--- business/src/main/java/com/rizomm/filemanager/business/entites/User.java
+++ business/src/main/java/com/rizomm/filemanager/business/entites/User.java
@@ -1,27 +1,24 @@
 package com.rizomm.filemanager.business.entites;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.lang.NonNull;
 
 @Entity
-@Builder
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
+    @NonNull
+    private String name;
 
-    private String password;
+    @Email
+    private String email;
+
 }
--- server/src/main/java/com/rizomm/filemanager/controllers/UserController.java
+++ server/src/main/java/com/rizomm/filemanager/controllers/UserController.java
@@ -1,20 +1,29 @@
 package com.rizomm.filemanager.controllers;
 
-import org.springframework.http.HttpStatus;
+import com.rizomm.filemanager.business.entites.User;
+import com.rizomm.filemanager.business.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.security.Principal;
+import java.util.List;
+
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestParam String username, @RequestParam String password) {
-        // TODO: check if user is valid user
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/me")
+    public String sayHello(Principal principal) {
+        return "Hello, " + principal.getName();
     }
 
 }
