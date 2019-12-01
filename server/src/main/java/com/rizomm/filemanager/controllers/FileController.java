0b86e25092c997816ffea8fccb3dbf02d261815e
Ahder <benzinered@yahoo.fr>
2019-12-01 20:54:07 +0100

    Update from minioService => minioClient

--- server/src/main/java/com/rizomm/filemanager/controllers/FileController.java
+++ server/src/main/java/com/rizomm/filemanager/controllers/FileController.java
@@ -1,61 +1,80 @@
 package com.rizomm.filemanager.controllers;
 
 import com.google.api.client.util.IOUtils;
-import com.jlefebure.spring.boot.minio.MinioException;
-import com.jlefebure.spring.boot.minio.MinioService;
+import io.minio.MinioClient;
+import io.minio.Result;
+import io.minio.errors.*;
 import io.minio.messages.Item;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.xmlpull.v1.XmlPullParserException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
 
-    @Autowired
-    private MinioService minioService;
+    private MinioClient minioClient;
 
-    @GetMapping
-    public List<Item> testMinio() {
-        return minioService.list();
+    public FileController() throws InvalidPortException, InvalidEndpointException {
+         minioClient = new MinioClient("http://127.0.0.1:9000/", "hello", "helloworld");
     }
 
-    @GetMapping("/{object}")
-    public void getObject(@PathVariable("object") String object, HttpServletResponse response) throws MinioException, IOException {
-        InputStream inputStream = minioService.get(Paths.get(object));
-        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+
+    @GetMapping("/uploadFile")
+    public void uploadFile(@RequestParam("file") MultipartFile file, @RequestParam String bucketName) throws IOException, XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, InvalidArgumentException, InvalidResponseException, InternalException, NoResponseException, InvalidBucketNameException, InsufficientDataException, ErrorResponseException {
+        minioClient.putObject(bucketName,  file.getOriginalFilename() , file.getInputStream(), file.getSize(), file.getContentType());
+    }
+
+    @GetMapping("/uploadFiles")
+    public void uploadFiles(@RequestParam("files") MultipartFile[] files, @RequestParam String bucketName) throws IOException, XmlPullParserException, NoSuchAlgorithmException, InvalidKeyException, InvalidArgumentException, InvalidResponseException, InternalException, NoResponseException, InvalidBucketNameException, InsufficientDataException, ErrorResponseException {
+        for (MultipartFile file: files) {
+            minioClient.putObject(bucketName,  file.getOriginalFilename() , file.getInputStream(), file.getSize(), file.getContentType());
+        }
+    }
+
+    @GetMapping("/downloadFile")
+    public void getObject(@RequestParam String objectName, @RequestParam String bucketName, HttpServletResponse response) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InvalidArgumentException, InvalidResponseException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException {
+        InputStream inputStream = minioClient.getObject(bucketName, objectName);
 
         // Set the content type and attachment header.
-        response.addHeader("Content-disposition", "attachment;filename=" + object);
-        response.setContentType(URLConnection.guessContentTypeFromName(object));
+        response.addHeader("Content-disposition", "attachment;filename=" + objectName);
+        response.setContentType(URLConnection.guessContentTypeFromName(objectName));
 
         // Copy the stream to the response's output stream.
         IOUtils.copy(inputStream, response.getOutputStream());
         response.flushBuffer();
     }
 
-    @PostMapping()
-    public void addAttachement(@RequestParam("file") MultipartFile file) {
-        Path path = Paths.get(file.getOriginalFilename());
-        Map<String, String> header = new HashMap<>();
-        header.put("X-Incident-Id", "C918371984");
-        try {
-            minioService.upload(path, file.getInputStream(), file.getContentType());
-        } catch (MinioException e) {
-            throw new IllegalStateException("The file cannot be upload on the internal storage. Please retry later", e);
-        } catch (IOException e) {
-            throw new IllegalStateException("The file cannot be read", e);
+    @GetMapping("/getAll")
+    public List<Item> getFiles(@RequestParam String bucketName) throws XmlPullParserException, InsufficientDataException, NoSuchAlgorithmException, IOException, NoResponseException, InvalidKeyException, InternalException, InvalidBucketNameException, ErrorResponseException {
+
+        Iterable<Result<Item>> myObjects = minioClient.listObjects(bucketName);
+        List<Item> items = new ArrayList<>();
+
+        for (Result<Item> result : myObjects) {
+            Item item = result.get();
+            items.add(item);
         }
+        return items;
     }
+
+    @GetMapping("/delete/{objectName}")
+    public void deleteObject(@PathVariable String objectName, @RequestParam String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InvalidResponseException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, InvalidArgumentException {
+         minioClient.removeObject(bucketName,objectName);
+    }
+
+    @GetMapping("/createBucket/{bucketName}")
+    public void createBucket(@PathVariable String bucketName) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InvalidResponseException, ErrorResponseException, NoResponseException, InvalidBucketNameException, XmlPullParserException, InternalException, RegionConflictException {
+        minioClient.makeBucket(bucketName);
+    }
+
 }
